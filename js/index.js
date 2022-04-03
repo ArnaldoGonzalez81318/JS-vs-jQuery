04794ff956b631782232c15ee6b0f28a95cfe483
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2022-04-03 16:44:57 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,7 @@
-// click to copy text
-function clickToCopyText(contentToCopy) {
-  // get text within the element specified by id
-  var textContent = document.getElementById(contentToCopy).innerText;
-  // write to clipboard
-  navigator.clipboard.writeText(textContent);
-
-  //tell user about it
-  alert(`copied`);
-}
+$(function () {
+  $(".fa-copy").click(function () {
+    $(this).closest(".lang-javascript").text();
+    document.execCommand("Copy");
+    console.log("Copied");
+  });
+});
