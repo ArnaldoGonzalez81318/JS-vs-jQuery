83deca01994a509df689fb4698b20ccfca957b39
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2022-04-12 18:04:57 -0400

    Adding side menu navigation

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const copyButtonLabel = "fa-solid fa-copy";
 let blocks = document.querySelectorAll(".copy-to-clipboard");
 
 blocks.forEach((block) => {
-  // only add button if browser supports Clipboard API
+  // only add button if browser supports Clipboard API.
   if (navigator.clipboard) {
     let button = document.createElement("icon");
     button.className = copyButtonLabel;
@@ -13,6 +13,7 @@ blocks.forEach((block) => {
   }
 });
 
+// copy code to clipboard function.
 async function copyCode(event) {
   const button = event.srcElement;
   const pre = button.parentElement.parentElement;
@@ -22,6 +23,7 @@ async function copyCode(event) {
 
   button.className = "fa-solid fa-circle-check";
 
+  // reset button after 1 seconds.
   setTimeout(() => {
     button.className = copyButtonLabel;
   }, 1000);
@@ -29,26 +31,27 @@ async function copyCode(event) {
 
 $(function () {
   $('.documentation-body h3').each(function () {
-    let $this = $(this);
-    let text = $this.text();
-    let sidebar = $('.sidebar-nav ul');
-    let li = $('<li class="nav-item"><a class="nav-link" href="' + $this.text().replace(/ /g, '') + '">' + text + '</a></li>');
+    let $this = $(this); // current h3
+    let text = $this.text(); // text of h3
+    let sidebar = $('.sidebar-nav ul'); // sidebar ul
+    let li = $('<li class="nav-item"><a class="nav-link" href="#' + $this.text().replace(/ /g, '') + '" data-scroll="' + text.replace(/ /g, '') + '">' + text + '</a></li>'); // create li
+
+    $('section').attr('data-anchor', text.replace(/ /g, ''));
 
     console.log(text);
 
+    // add li to sidebar
     sidebar.append(li);
-  });
 
-  $(".nav-link").click(function (e) {
-    e.preventDefault();
+    $('.nav-link').click(function () {
+      let scrollAnchor = $(this).attr('data-scroll');
+      let scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 25;
 
-    let target = $(this).attr("href");
+      $('.document').animate({
+        scrollTop: scrollPoint
+      }, 500);
 
-    $('.nav-link').removeClass('active-link');
-    $(this).addClass('active-link');
-
-    $('#documentation-body').animate({
-      scrollTop: target.top - 20
-    }, 500);
+      return false;
+    });
   });
 });
