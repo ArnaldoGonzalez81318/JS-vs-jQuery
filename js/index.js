85f5ace79f36224e0729952733c5a5e7261d877f
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2022-04-13 09:30:56 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,22 +31,23 @@ async function copyCode(event) {
 
 $(function () {
   $('.documentation-body h3').each(function () {
-    let $this = $(this); // current h3
-    let text = $this.text(); // text of h3
-    let sidebar = $('.sidebar-nav ul'); // sidebar ul
-    let li = $('<li class="nav-item"><a class="nav-link" href="#' + $this.text().replace(/ /g, '') + '" data-scroll="' + text.replace(/ /g, '') + '">' + text + '</a></li>'); // create li
+    let $this = $(this); // current h3.
+    let text = $this.text(); // text of h3.
+    let sidebar = $('.sidebar-nav ul'); // sidebar ul.
+    let li = $('<li class="nav-item"><a class="nav-link" href="#' + $this.text().replace(/ /g, '') + '" data-scroll="' + text.replace(/ /g, '') + '">' + text + '</a></li>'); // create li.
 
     $('section').attr('data-anchor', text.replace(/ /g, ''));
 
     console.log(text);
 
-    // add li to sidebar
+    // add li to sidebar.
     sidebar.append(li);
 
     $('.nav-link').click(function () {
       let scrollAnchor = $(this).attr('data-scroll');
-      let scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 25;
+      let scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 125;
 
+      // scroll to section.
       $('.document').animate({
         scrollTop: scrollPoint
       }, 500);
