4e194450e4eb1e5c1d9a07396b5299fc550cfac0
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2022-06-02 16:44:49 -0400

    Adding scroll progress bar

--- js/index.js
+++ js/index.js
@@ -113,3 +113,14 @@ $(function () {
     return false;
   });
 });
+
+window.onscroll = function () {
+  progressIndicator();
+};
+
+function progressIndicator() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
