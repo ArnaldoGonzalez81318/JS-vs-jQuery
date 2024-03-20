ad25cffeb7d8cd2210b0e1c2472fe39cdd08d850
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2024-03-20 10:06:18 -0400

    Update themeSwitcher.js

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -1,7 +1,28 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  console.log('dark mode')
-  document.body.classList.add('dark-mode')
-} else {
-  console.log('light mode')
-  document.body.classList.remove('dark-mode')
+const toggleSwitch = document.getElementById('checkbox');
+
+// Function to set the initial theme based on system preferences
+function setInitialTheme() {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // Dark mode preferred
+    toggleSwitch.checked = true;
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    // Light mode preferred
+    toggleSwitch.checked = false;
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
 }
+
+// Event listener for theme switch toggle
+toggleSwitch.addEventListener('change', function () {
+  if (this.checked) {
+    console.log('Dark theme');
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    console.log('Light theme');
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+});
+
+// Set initial theme
+setInitialTheme();
